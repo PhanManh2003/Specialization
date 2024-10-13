45f8d4a949f0c655d0aea711bf1afa91a37421b1
Phan Manh <manhpthe172481@fpt.edu.vn>
2024-10-13 14:39:21 +0700

    newest

---
+++ 24_fall/FER202/DEMO_FER2024_SE1871/JsonServerDemo/src/App.js
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from "react";
+
+import InformationDisclosure from "./InformationDisclosure";
+import ListOfInformation from "./ListOfInformation";
+import qualities from "./given/Qualities";
+
+const App = () => {
+  const [dat, setData] = useState([]);
+
+  const callFetchApi = async () => {
+    try {
+      const response = await fetch("http://localhost:9000/data");
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const createNew = (data, callback) => {
+    let obj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    fetch("http://localhost:9000/data", obj)
+      .then((response) => {
+        response.json();
+      })
+      .then(callback);
+  };
+
+  useEffect(() => {
+    callFetchApi();
+  }, []);
+
+  const addFunc = (name, date, tech, courses) => {
+    const [year, month, day] = date.split("-");
+    const newObj = {
+      name: name,
+      dob: `${day}/${month}/${year}`,
+      courses: courses.map((course) => ({
+        name: course,
+      })),
+      qualities: tech,
+    };
+    createNew(newObj, () => {
+      callFetchApi();
+    });
+    // setData((prev) => [
+    //   ...prev,
+    //   {
+    //     name: name,
+    //     dob: `${day}/${month}/${year}`,
+    //     courses: courses.map((course) => ({
+    //       name: course,
+    //     })),
+    //     qualities: tech,
+    //   },
+    // ]);
+  };
+
+  const deleteFunc = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:9000/data/${id}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        setData((prev) => prev.filter((item) => item.id !== id));
+      } else {
+        throw new Error("Fail");
+      }
+    } catch (error) {
+      console.error("cannot delete data");
+    }
+  };
+
+  const searchFunc = (name) => {
+    if (!!name) {
+      let newData;
+      newData = [
+        ...dat.filter((item) =>
+          item?.name?.toLowerCase()?.includes(name?.toLowerCase())
+        ),
+      ];
+      setData(newData);
+    } else {
+      callFetchApi();
+    }
+  };
+
+  return (
+    <div className="container my-5">
+      <InformationDisclosure addFunc={addFunc} searchFunc={searchFunc} />
+      <ListOfInformation data={dat ?? []} deleteFunc={deleteFunc} />
+    </div>
+  );
+};
+
+export default App;
---
+++ 24_fall/FER202/DEMO_FER2024_SE1871/JsonServerDemo/src/given/Qualities.js
@@ -0,0 +1,2 @@
+const qualities = ["Very good", "Good", "Nordgdsmal", "Bad"];
+export default qualities;
